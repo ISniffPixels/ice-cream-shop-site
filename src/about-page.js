31d6888a5641be253ddab2620470b1d4b11ac2e6
Hugo <hugowinged@gmail.com>
2025-03-31 19:58:31 -0700

    ...

--- src/about-page.js
+++ src/about-page.js
@@ -1,8 +1,11 @@
 export function aboutPage() {
     const content_container = document.createElement('main');
+    const h1 = document.createElement('h1');
 
     document.querySelector('#content').innerHTML = "";
     content_container.classList.add('about_container_style');
+    h1.textContent = "Hello World!";
+    content_container.appendChild(h1);
    
     document.querySelector('#content').appendChild(content_container);
 }
