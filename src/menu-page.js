cf4654a0639ac19cc99c8f13f9a87f2e62946a34
Hugo <hugowinged@gmail.com>
2025-04-05 19:58:49 -0700

    Added some logic changes

--- src/menu-page.js
+++ src/menu-page.js
@@ -13,7 +13,7 @@ export function menuPage() {
     const menu_box_9 = document.createElement('div');
 
     const menu_heading = document.createElement('h1');
-    menu_heading.textContent = "Flavor Station"
+    menu_heading.textContent = "Flavor Station Baby!"
     menu_heading.style.textAlign = 'center';
     menu_heading.style.fontSize = '5.5rem';
     menu_heading.style.paddingTop = '30px';
